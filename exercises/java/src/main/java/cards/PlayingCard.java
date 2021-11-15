7251a16023a1b54aa8bed7d289d04927004d1a1c
Philip <philip.ingham.phs@gmail.com>
2021-11-15 14:25:10 +0000

    Modify PlayingCard

--- exercises/java/src/main/java/cards/PlayingCard.java
+++ exercises/java/src/main/java/cards/PlayingCard.java
@@ -1,10 +1,10 @@
 package cards;
 
 public class PlayingCard {
-    private final int suit;
+    private final Suit suit;
     private final int faceValue;
 
-    public PlayingCard(int suit, int faceValue) {
+    public PlayingCard(Suit suit, int faceValue) {
         this.suit = suit;
         this.faceValue = faceValue;
     }
