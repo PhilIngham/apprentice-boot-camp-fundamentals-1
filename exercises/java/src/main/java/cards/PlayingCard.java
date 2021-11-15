af6aa4a60f7f9e8c502c29aa8259d826a8931d4d
Philip <philip.ingham.phs@gmail.com>
2021-11-15 14:24:14 +0000

    Add PlayingCard and begin Suit

---
+++ exercises/java/src/main/java/cards/PlayingCard.java
@@ -0,0 +1,11 @@
+package cards;
+
+public class PlayingCard {
+    private final int suit;
+    private final int faceValue;
+
+    public PlayingCard(int suit, int faceValue) {
+        this.suit = suit;
+        this.faceValue = faceValue;
+    }
+}
---
+++ exercises/java/src/main/java/cards/Suit.java
@@ -0,0 +1,9 @@
+package cards;
+
+public class Suit {
+    private final String suit;
+
+    public Suit(String suit) {
+        this.suit = suit;
+    }
+}
