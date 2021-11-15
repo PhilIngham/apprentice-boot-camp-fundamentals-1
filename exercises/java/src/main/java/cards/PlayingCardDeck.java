2022f2b5cb8be2e2279c659bd7a5bc0766d638c9
Philip <philip.ingham.phs@gmail.com>
2021-11-15 14:27:58 +0000

    Add PlayingCardDeck

---
+++ exercises/java/src/main/java/cards/PlayingCardDeck.java
@@ -0,0 +1,12 @@
+package cards;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayingCardDeck {
+    private List<PlayingCard> cards;
+
+    public PlayingCardDeck(){
+        this.cards = new ArrayList<>();
+    }
+}
